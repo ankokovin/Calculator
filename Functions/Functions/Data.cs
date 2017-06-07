99b6f98eedfb174be3b3beddc82d49267ddd44de
Коковин Алексей Николаевич <ankokovin@edu.hse.ru>
2017-06-07 11:05:09 +0500

    Основа Data

--- Functions/Functions/Data.cs
+++ Functions/Functions/Data.cs
@@ -8,5 +8,39 @@ namespace Functions
 {
     class Data
     {
+        const int N = 5;
+        uint[] Digits = new uint[N];
+        bool Plus = true;
+        const uint Base = 100000;
+        public static Data operator + (Data first, Data second)
+        {
+            throw new NotImplementedException();
+        }
+        public static Data operator -(Data first, Data second)
+        {
+            throw new NotImplementedException();
+        }
+        public static Data operator *(Data first, Data second)
+        {
+            throw new NotImplementedException();
+        }
+        public static Data operator /(Data first, Data second)
+        {
+            throw new NotImplementedException();
+        }
+        public static double Divide (Data first, Data second)
+        {
+            throw new NotImplementedException();
+        }
+       
+        public static double operator %(Data first, Data second)
+        {
+            throw new NotImplementedException();
+        }
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+            return base.ToString();
+        }
     }
 }
