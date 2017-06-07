b54d326a4fcfe8b3c11a1cb63c60eb8a1968bdda
IskusnikXD <IskusnikXD@ISKUSNIK>
2017-06-07 12:30:19 +0500

    1. Проект формы

---
+++ Form/CalcFormApp/MenuForm.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace CalcFormApp
+{
+    partial class MenuForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Add1 = new System.Windows.Forms.Button();
+            this.Add2 = new System.Windows.Forms.Button();
+            this.Add3 = new System.Windows.Forms.Button();
+            this.Add6 = new System.Windows.Forms.Button();
+            this.Add5 = new System.Windows.Forms.Button();
+            this.Add4 = new System.Windows.Forms.Button();
+            this.Add9 = new System.Windows.Forms.Button();
+            this.Add8 = new System.Windows.Forms.Button();
+            this.Add7 = new System.Windows.Forms.Button();
+            this.Plus = new System.Windows.Forms.Button();
+            this.Minus = new System.Windows.Forms.Button();
+            this.DivideInt = new System.Windows.Forms.Button();
+            this.Multiply = new System.Windows.Forms.Button();
+            this.Rest = new System.Windows.Forms.Button();
+            this.Clear = new System.Windows.Forms.Button();
+            this.Previous = new System.Windows.Forms.Button();
+            this.DivideFloat = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Add1
+            // 
+            this.Add1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add1.Location = new System.Drawing.Point(13, 72);
+            this.Add1.Name = "Add1";
+            this.Add1.Size = new System.Drawing.Size(45, 45);
+            this.Add1.TabIndex = 0;
+            this.Add1.Text = "1";
+            this.Add1.UseVisualStyleBackColor = true;
+            this.Add1.Click += new System.EventHandler(this.Add1_Click);
+            // 
+            // Add2
+            // 
+            this.Add2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add2.Location = new System.Drawing.Point(64, 72);
+            this.Add2.Name = "Add2";
+            this.Add2.Size = new System.Drawing.Size(45, 45);
+            this.Add2.TabIndex = 1;
+            this.Add2.Text = "2";
+            this.Add2.UseVisualStyleBackColor = true;
+            this.Add2.Click += new System.EventHandler(this.Add2_Click);
+            // 
+            // Add3
+            // 
+            this.Add3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add3.Location = new System.Drawing.Point(115, 72);
+            this.Add3.Name = "Add3";
+            this.Add3.Size = new System.Drawing.Size(45, 45);
+            this.Add3.TabIndex = 2;
+            this.Add3.Text = "3";
+            this.Add3.UseVisualStyleBackColor = true;
+            // 
+            // Add6
+            // 
+            this.Add6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add6.Location = new System.Drawing.Point(115, 123);
+            this.Add6.Name = "Add6";
+            this.Add6.Size = new System.Drawing.Size(45, 45);
+            this.Add6.TabIndex = 5;
+            this.Add6.Text = "6";
+            this.Add6.UseVisualStyleBackColor = true;
+            this.Add6.Click += new System.EventHandler(this.Add6_Click);
+            // 
+            // Add5
+            // 
+            this.Add5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add5.Location = new System.Drawing.Point(64, 123);
+            this.Add5.Name = "Add5";
+            this.Add5.Size = new System.Drawing.Size(45, 45);
+            this.Add5.TabIndex = 4;
+            this.Add5.Text = "5";
+            this.Add5.UseVisualStyleBackColor = true;
+            this.Add5.Click += new System.EventHandler(this.Add5_Click);
+            // 
+            // Add4
+            // 
+            this.Add4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add4.Location = new System.Drawing.Point(13, 123);
+            this.Add4.Name = "Add4";
+            this.Add4.Size = new System.Drawing.Size(45, 45);
+            this.Add4.TabIndex = 3;
+            this.Add4.Text = "4";
+            this.Add4.UseVisualStyleBackColor = true;
+            this.Add4.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Add9
+            // 
+            this.Add9.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add9.Location = new System.Drawing.Point(115, 174);
+            this.Add9.Name = "Add9";
+            this.Add9.Size = new System.Drawing.Size(45, 45);
+            this.Add9.TabIndex = 8;
+            this.Add9.Text = "9";
+            this.Add9.UseVisualStyleBackColor = true;
+            this.Add9.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Add8
+            // 
+            this.Add8.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add8.Location = new System.Drawing.Point(64, 174);
+            this.Add8.Name = "Add8";
+            this.Add8.Size = new System.Drawing.Size(45, 45);
+            this.Add8.TabIndex = 7;
+            this.Add8.Text = "8";
+            this.Add8.UseVisualStyleBackColor = true;
+            this.Add8.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Add7
+            // 
+            this.Add7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Add7.Location = new System.Drawing.Point(13, 174);
+            this.Add7.Name = "Add7";
+            this.Add7.Size = new System.Drawing.Size(45, 45);
+            this.Add7.TabIndex = 6;
+            this.Add7.Text = "7";
+            this.Add7.UseVisualStyleBackColor = true;
+            this.Add7.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // Plus
+            // 
+            this.Plus.BackColor = System.Drawing.Color.Coral;
+            this.Plus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Plus.Location = new System.Drawing.Point(166, 72);
+            this.Plus.Name = "Plus";
+            this.Plus.Size = new System.Drawing.Size(45, 45);
+            this.Plus.TabIndex = 9;
+            this.Plus.Text = "+";
+            this.Plus.UseVisualStyleBackColor = false;
+            this.Plus.Click += new System.EventHandler(this.Plus_Click);
+            this.Plus.MouseHover += new System.EventHandler(this.plusInfo);
+            // 
+            // Minus
+            // 
+            this.Minus.BackColor = System.Drawing.Color.Coral;
+            this.Minus.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Minus.Location = new System.Drawing.Point(217, 72);
+            this.Minus.Name = "Minus";
+            this.Minus.Size = new System.Drawing.Size(45, 45);
+            this.Minus.TabIndex = 10;
+            this.Minus.Text = "-";
+            this.Minus.UseVisualStyleBackColor = false;
+            this.Minus.Click += new System.EventHandler(this.Minus_Click);
+            // 
+            // DivideInt
+            // 
+            this.DivideInt.BackColor = System.Drawing.Color.Coral;
+            this.DivideInt.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.DivideInt.Location = new System.Drawing.Point(217, 123);
+            this.DivideInt.Name = "DivideInt";
+            this.DivideInt.Size = new System.Drawing.Size(45, 45);
+            this.DivideInt.TabIndex = 11;
+            this.DivideInt.Text = "Div";
+            this.DivideInt.UseVisualStyleBackColor = false;
+            this.DivideInt.Click += new System.EventHandler(this.DivideInt_Click);
+            this.DivideInt.MouseHover += new System.EventHandler(this.button1_MouseHover);
+            // 
+            // Multiply
+            // 
+            this.Multiply.BackColor = System.Drawing.Color.Coral;
+            this.Multiply.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Multiply.Location = new System.Drawing.Point(166, 123);
+            this.Multiply.Name = "Multiply";
+            this.Multiply.Size = new System.Drawing.Size(45, 45);
+            this.Multiply.TabIndex = 12;
+            this.Multiply.Text = "*";
+            this.Multiply.UseVisualStyleBackColor = false;
+            this.Multiply.Click += new System.EventHandler(this.Multiply_Click);
+            this.Multiply.MouseHover += new System.EventHandler(this.Multiply_MouseHover);
+            // 
+            // Rest
+            // 
+            this.Rest.BackColor = System.Drawing.Color.Coral;
+            this.Rest.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Rest.Location = new System.Drawing.Point(268, 72);
+            this.Rest.Name = "Rest";
+            this.Rest.Size = new System.Drawing.Size(45, 45);
+            this.Rest.TabIndex = 13;
+            this.Rest.Text = "%";
+            this.Rest.UseVisualStyleBackColor = false;
+            this.Rest.Click += new System.EventHandler(this.Rest_Click);
+            this.Rest.MouseHover += new System.EventHandler(this.Rest_MouseHover);
+            // 
+            // Clear
+            // 
+            this.Clear.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Clear.Location = new System.Drawing.Point(166, 174);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(45, 45);
+            this.Clear.TabIndex = 14;
+            this.Clear.Text = "C";
+            this.Clear.UseVisualStyleBackColor = false;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            this.Clear.MouseHover += new System.EventHandler(this.Clear_MouseHover);
+            // 
+            // Previous
+            // 
+            this.Previous.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Previous.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.Previous.Location = new System.Drawing.Point(217, 174);
+            this.Previous.Name = "Previous";
+            this.Previous.Size = new System.Drawing.Size(45, 45);
+            this.Previous.TabIndex = 15;
+            this.Previous.Text = "←";
+            this.Previous.UseVisualStyleBackColor = false;
+            this.Previous.Click += new System.EventHandler(this.Previous_Click);
+            this.Previous.MouseHover += new System.EventHandler(this.Previous_MouseHover);
+            // 
+            // DivideFloat
+            // 
+            this.DivideFloat.BackColor = System.Drawing.Color.Coral;
+            this.DivideFloat.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.DivideFloat.Location = new System.Drawing.Point(268, 123);
+            this.DivideFloat.Name = "DivideFloat";
+            this.DivideFloat.Size = new System.Drawing.Size(45, 45);
+            this.DivideFloat.TabIndex = 16;
+            this.DivideFloat.Text = "/";
+            this.DivideFloat.UseVisualStyleBackColor = false;
+            this.DivideFloat.Click += new System.EventHandler(this.DivideFloat_Click);
+            this.DivideFloat.MouseHover += new System.EventHandler(this.DivideFloat_MouseHover);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(300, 53);
+            this.richTextBox1.TabIndex = 17;
+            this.richTextBox1.Text = "";
+            // 
+            // MenuForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 234);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.DivideInt);
+            this.Controls.Add(this.DivideFloat);
+            this.Controls.Add(this.Plus);
+            this.Controls.Add(this.Add9);
+            this.Controls.Add(this.Previous);
+            this.Controls.Add(this.Add8);
+            this.Controls.Add(this.Minus);
+            this.Controls.Add(this.Add7);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.Add6);
+            this.Controls.Add(this.Multiply);
+            this.Controls.Add(this.Add5);
+            this.Controls.Add(this.Rest);
+            this.Controls.Add(this.Add4);
+            this.Controls.Add(this.Add3);
+            this.Controls.Add(this.Add2);
+            this.Controls.Add(this.Add1);
+            this.Name = "MenuForm";
+            this.Text = "Калькулятор длинной арифметики";
+            this.Load += new System.EventHandler(this.MenuForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Add1;
+        private System.Windows.Forms.Button Add2;
+        private System.Windows.Forms.Button Add3;
+        private System.Windows.Forms.Button Add6;
+        private System.Windows.Forms.Button Add5;
+        private System.Windows.Forms.Button Add4;
+        private System.Windows.Forms.Button Add9;
+        private System.Windows.Forms.Button Add8;
+        private System.Windows.Forms.Button Add7;
+        private System.Windows.Forms.Button Plus;
+        private System.Windows.Forms.Button Minus;
+        private System.Windows.Forms.Button DivideInt;
+        private System.Windows.Forms.Button Multiply;
+        private System.Windows.Forms.Button Rest;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button Previous;
+        private System.Windows.Forms.Button DivideFloat;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
--- Form/CalcFormApp/Program.cs
+++ Form/CalcFormApp/Program.cs
@@ -15,7 +15,7 @@ namespace CalcFormApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MenuForm());
         }
     }
 }
