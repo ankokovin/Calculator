97f4b345e60b1ce7eae38cd8e8f92e36f948b26a
Коковин Алексей Николаевич <ankokovin@edu.hse.ru>
2017-06-07 11:23:05 +0500

    Начало конструтора, + и немного другого

--- Functions/Functions/Data.cs
+++ Functions/Functions/Data.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Functions
@@ -12,8 +12,45 @@ namespace Functions
         uint[] Digits = new uint[N];
         bool Plus = true;
         const uint Base = 100000;
+        public Data(string Input)
+        {
+            if (Input.Length > 26) throw new Exception("Слишком большая строка");
+            Regex check = new Regex("^-?[0-9]+$");
+            if (!check.IsMatch(Input)) throw new Exception("Неверное представление");
+            throw new NotImplementedException();
+        }
+        public static bool operator < (Data first, Data second)
+        {
+            throw new NotImplementedException();
+        }
+        public static bool operator >(Data first, Data second)
+        {
+            throw new NotImplementedException();
+        }
+        public Data Copy()
+        {
+            throw new NotImplementedException();
+        }
         public static Data operator + (Data first, Data second)
         {
+            if (first.Plus != second.Plus)
+            {
+                Data f;
+                Data s;
+                if (first > second)
+                {
+                    f = first.Copy();
+                    s = second.Copy();
+
+                }
+                else
+                {
+                    f = second.Copy();
+                    s = first.Copy();
+                }
+                s.Plus = !s.Plus;
+                return f - s;
+            }
             throw new NotImplementedException();
         }
         public static Data operator -(Data first, Data second)
